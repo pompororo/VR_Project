8ac7aa4dbde87fba78ce847236c840113ca90972
phiraphonnn <65663938+phiraphonnn@users.noreply.github.com>
2023-11-19 05:02:49 +0700

    still kuy i dont want to use Unity VFX Grpah

--- Assets/ElectricArc.cs
+++ Assets/ElectricArc.cs
@@ -2,25 +2,22 @@ using UnityEngine;
 
 public class ElectricArc : MonoBehaviour
 {
-    public Transform startPoint; // Set the start position in the Inspector
-    public Transform endPoint;   // Set the end position in the Inspector
-    public float speed = 1f;     // Set the movement speed in the Inspector
+    public GameObject startObject;
+    public GameObject endObject;
 
-    private ParticleSystem particleSystem;
-
-    void Start()
+    private void Start()
     {
-        particleSystem = GetComponent<ParticleSystem>();
-    }
+        if (startObject != null)
+        {
+            // Set the initial position of the electric arc to the position of the start object
+            transform.position = startObject.transform.position;
+        }
 
-    void Update()
-    {
-        MoveParticleSystem();
-    }
-
-    void MoveParticleSystem()
-    {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, Mathf.PingPong(Time.time * step, 1));
+        // Set the end position of the electric arc
+        if (endObject != null)
+        {
+            ParticleSystem.ShapeModule shapeModule = GetComponent<ParticleSystem>().shape;
+            shapeModule.position = endObject.transform.position;
+        }
     }
 }
