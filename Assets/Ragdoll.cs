bfa124a24aa9214fa29fd591b3eac1803ced9084
ZetsiShineru <praweena.sare@bumail.net>
2023-11-28 14:40:18 +0700

    ragdoll script

---
+++ Assets/Ragdoll.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    public BoxCollider MainCollider;
+    public GameObject cloonRig;
+    public Animator cloonAni;
+    
+    void Start()
+    {
+        GetRagDollBits();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Lightsaber")
+        {
+            RagdollModeOn();
+        }
+    }
+
+    private Collider[] ragDollColliders;
+    private Rigidbody[] limbsRigidbodies;
+    
+    void GetRagDollBits()
+    {
+        ragDollColliders = cloonRig.GetComponentsInChildren<Collider>();
+        limbsRigidbodies = cloonRig.GetComponentsInChildren<Rigidbody>();
+    }
+
+    void RagdollModeOn()
+    {
+        cloonAni.enabled = false;
+        
+        foreach (Collider col in ragDollColliders)
+        {
+            col.enabled = true;
+        }
+        
+        foreach (Rigidbody rigid in limbsRigidbodies)
+        {
+            rigid.isKinematic = false;
+        }
+        
+        MainCollider.enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void RagdollModeOff()
+    {
+        foreach (Collider col in ragDollColliders)
+        {
+            col.enabled = false;
+        }
+        
+        foreach (Rigidbody rigid in limbsRigidbodies)
+        {
+            rigid.isKinematic = true;
+        }
+
+        cloonAni.enabled = true;
+        MainCollider.enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
